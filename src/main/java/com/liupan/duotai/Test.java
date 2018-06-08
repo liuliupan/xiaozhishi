0a58972c47e3ff8546476669ad05f594053b3edc
liupa <liupa@gwall-lp>
2018-06-08 16:01:40 +0800

    22222222222222222222222

---
+++ src/main/java/com/liupan/duotai/Test.java
@@ -0,0 +1,18 @@
+package com.liupan.duotai;
+public class Test {
+    public static void main(String[] args) {
+        Wine a = new JNC();
+        a.fun1();
+        
+//        JNC jnc = new JNC();
+//        Wine b=(Wine)jnc;
+//        b.fun1();
+//        
+//        Wine c = new Wine();
+//        c.fun1();
+    }
+}
+//-------------------------------------------------
+//Output:
+//Wine 的Fun.....
+//JNC 的Fun2...
