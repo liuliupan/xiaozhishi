c019c5fabf6f7cc94e22138569e5bf1f426c07bc
liupa <liupa@gwall-lp>
2018-06-11 17:27:34 +0800

    444444444444444444444444444444

--- src/main/java/com/liupan/fenbusuo/redis/Service.java
+++ src/main/java/com/liupan/fenbusuo/redis/Service.java
@@ -12,9 +12,9 @@ public class Service {
     static {
         JedisPoolConfig config = new JedisPoolConfig();
         // 设置最大连接数
-        config.setMaxTotal(200);
+        config.setMaxTotal(3);
         // 设置最大空闲数
-        config.setMaxIdle(8);
+        config.setMaxIdle(2);
         // 设置最大等待时间
         config.setMaxWaitMillis(1000 * 100);
         // 在borrow一个jedis实例时，是否需要验证，若为true，则所有jedis实例均是可用的
@@ -28,8 +28,8 @@ public class Service {
 
     public void seckill() {
         // 返回锁的value值，供释放锁时候进行判断
-        String indentifier = lock.lockWithTimeout("resource", 5000, 1000);
-        System.out.println(Thread.currentThread().getName() + "获得了锁");
+        String indentifier = lock.lockWithTimeout("resource", 5000, 3000);
+        System.out.println("UUID"+indentifier+Thread.currentThread().getName() + "获得了锁");
         System.out.println(--n);
         lock.releaseLock("resource", indentifier);
     }
